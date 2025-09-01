8ef322a22832e21c746a89377bdb2d6996af0b64
thiagodeas <sousaalvesth@gmail.com>
2025-09-01 19:39:18 -0300

    initializing project

---
+++ src/test/java/com/thiagodeas/javastockmanager/JavastockmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagodeas.javastockmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavastockmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
