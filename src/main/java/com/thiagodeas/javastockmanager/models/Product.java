4bf1deba145747aaccc97d15066d4de28dcaed28
thiagodeas <sousaalvesth@gmail.com>
2025-09-01 20:19:00 -0300

    feat(model): add product model

---
+++ src/main/java/com/thiagodeas/javastockmanager/models/Product.java
@@ -0,0 +1,81 @@
+package com.thiagodeas.javastockmanager.models;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String category;
+
+    @NotBlank
+    private BigDecimal price;
+
+    public Product (String name, String category, BigDecimal price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+
+        return id == product.id &&
+               Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", category='" + category + '\'' +
+        ", price=" + price +
+        '}';
+    }
+}
