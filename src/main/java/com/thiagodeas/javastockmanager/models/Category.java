5902b1a1d51beb87c6b3e0c0540cb459a366190d
thiagodeas <sousaalvesth@gmail.com>
2025-09-02 13:48:53 -0300

    refactor(category): add unique constraint to name column

--- src/main/java/com/thiagodeas/javastockmanager/models/Category.java
+++ src/main/java/com/thiagodeas/javastockmanager/models/Category.java
@@ -2,6 +2,7 @@ package com.thiagodeas.javastockmanager.models;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(unique = true)
     @NotBlank
     private String name;
 
