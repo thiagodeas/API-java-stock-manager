e5cd2b5aade6f851e02b3e0d292d125139ddaf15
thiagodeas <sousaalvesth@gmail.com>
2025-09-02 14:11:44 -0300

    feat(repository): create outbound repository

---
+++ src/main/java/com/thiagodeas/javastockmanager/repositories/OutboundRepository.java
@@ -0,0 +1,15 @@
+package com.thiagodeas.javastockmanager.repositories;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.thiagodeas.javastockmanager.models.Outbound;
+import com.thiagodeas.javastockmanager.models.enums.OutboundReason;
+
+public interface OutboundRepository extends JpaRepository<Outbound, Integer>{
+    List<Outbound> findByDateBetween(LocalDateTime start, LocalDateTime end);
+    List<Outbound> findByReason(OutboundReason reason);
+    List<Outbound> findByDateBetweenAndReason(LocalDateTime start, LocalDateTime end, OutboundReason reason);
+}
