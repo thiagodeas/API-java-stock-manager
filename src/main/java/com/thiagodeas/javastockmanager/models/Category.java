d53099e52a62231f87564889fa6a2dab633a3310
thiagodeas <sousaalvesth@gmail.com>
2025-09-02 12:57:56 -0300

    feat(model): add category model

---
+++ src/main/java/com/thiagodeas/javastockmanager/models/Category.java
@@ -0,0 +1,58 @@
+package com.thiagodeas.javastockmanager.models;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+
+        return id == category.id &&
+            Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+        "id=" + id +
+        ", name='" + name +
+        '}';
+    }
+}
