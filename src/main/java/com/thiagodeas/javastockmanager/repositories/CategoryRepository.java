4ec6763dfcb7a04e4845f0779c11c00988322529
thiagodeas <sousaalvesth@gmail.com>
2025-09-04 10:13:03 -0300

    feat(repository): add find by name containing method

--- src/main/java/com/thiagodeas/javastockmanager/repositories/CategoryRepository.java
+++ src/main/java/com/thiagodeas/javastockmanager/repositories/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.thiagodeas.javastockmanager.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.thiagodeas.javastockmanager.models.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     Category findByName(String name);
+    List<Category> findByNameContaining(String name);
 }
