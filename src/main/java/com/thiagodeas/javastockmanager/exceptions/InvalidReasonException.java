ef3f2f25478681e21db9e8846201c9446084c247
thiagodeas <sousaalvesth@gmail.com>
2025-09-05 13:22:34 -0300

    feat(exception): add invalid reason exception class

---
+++ src/main/java/com/thiagodeas/javastockmanager/exceptions/InvalidReasonException.java
@@ -0,0 +1,7 @@
+package com.thiagodeas.javastockmanager.exceptions;
+
+public class InvalidReasonException extends RuntimeException {
+    public InvalidReasonException(String message) {
+        super(message);
+    }
+}
