8468b819763deea5341e6b79960d024fe6200e41
thiagodeas <sousaalvesth@gmail.com>
2025-09-05 13:22:08 -0300

    feat(exception): add invalid date range exception

---
+++ src/main/java/com/thiagodeas/javastockmanager/exceptions/InvalidDateRangeException.java
@@ -0,0 +1,8 @@
+package com.thiagodeas.javastockmanager.exceptions;
+
+public class InvalidDateRangeException extends RuntimeException {
+    public InvalidDateRangeException(String message) {
+        super(message);
+    }
+    
+}
